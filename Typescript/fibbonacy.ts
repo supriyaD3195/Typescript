ffe4c95b857271bc31f32fac69eedc245c9d3676
supriya nandkumar dharmadhikari <62790793+supriyaD3195@users.noreply.github.com>
2020-03-28 17:16:53 +0530

    Add files via upload

---
+++ Typescript/fibbonacy.ts
@@ -0,0 +1,12 @@
+var n=5;
+var a,b,c;
+a=0;
+b=1;
+for(var i=0;i<=n;i++)
+{
+    c=a+b;
+    console.log("fibonacy series are:  "+c);
+    a=b;
+    b=c;
+
+}
---
+++ Typescript/swap.js
@@ -0,0 +1,8 @@
+var a, b;
+a = 10;
+b = 20;
+a = a + b;
+b = a - b;
+a = a - b;
+console.log("a=" + a);
+console.log("b=" + b);
